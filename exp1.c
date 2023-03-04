2c933fe160c89d5dcb83a5b63d3f6be9408d7ddc
Aparup Roychowdhury <aparuproychowdhury@gmail.com>
2023-03-04 22:46:49 +0530

    First commit

---
+++ exp1.c
@@ -0,0 +1,54 @@
+#include<stdio.h>
+//int main(){
+//    char name[]="Aparup Roychowdhury",dept[]="BSH";
+//    int roll=25;
+//    printf("Name: %s, Deparment: %s, Roll: %d", name,dept,roll);
+//    return 0;
+//}
+
+//int main(){
+//  int a,b,s,d;
+//  printf("Enter Two numbers: ");
+//  scanf("%d %d",&a,&b);
+//  s=a+b;
+//  d=a-b;
+//  printf("Sum of the two numbers is: %d \n",s);
+//  printf("Difference between the two numbers is: %d \n",d);
+//
+//}
+
+//int main(){
+//  int a=15,b=17,s,d;
+//  s=a+b;
+//  d=a-b;
+//  printf("The Sum of the two numbers is:%d\n",s);
+// printf("The Difference between the numbers is:%d\n",d);
+//
+//}
+
+//int main(){
+//  int a, b, c, p;
+//  printf("Enter Three numbers: ");
+//  scanf("%d %d %d",&a, &b, &c);
+//  p=a*b*c;
+//  printf("The product of the three numbers: %d\n ",p);
+//  return 0;
+//}
+
+//int main(){
+//  int a, b, c, d, e, s, avg;
+//  printf("Enter five numbers: ");
+//  scanf("%d %d %d %d %d",&a, &b, &c, &d, &e);
+//  s=a+b+c+d+e;
+//  avg=s/5;
+//  printf("The sum is: %d\nThe Average is: %d\n",s,avg);
+//}
+
+int main(){
+int a,b,p;
+printf("Enter the length and breadth: ");
+scanf("%d %d",&a,&b);
+p=2*(a+b);
+printf("The perimeter of the rectangle is: %d\n",p);
+}
+
